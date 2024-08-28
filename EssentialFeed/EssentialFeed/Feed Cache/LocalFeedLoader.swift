475cd6fd218e01341a0ac4ec3d6bb3f738120ce1
ChenZhen <chenzhennba@gmail.com>
2024-08-28 17:45:38 +1000

    Add SaveResult type alias to protect code from potential breaking changes

--- EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -10,13 +10,15 @@ import Foundation
 public class LocalFeedLoader {
     private let store: FeedStore
     private let currentDate: () -> Date
+    
+    public typealias SaveResult = Error?
 
     public init(store: FeedStore, currentDate: @escaping () -> Date) {
         self.store = store
         self.currentDate = currentDate
     }
     
-    public func save(_ items: [FeedItem], completion: @escaping (Error?) -> Void) {
+    public func save(_ items: [FeedItem], completion: @escaping (SaveResult) -> Void) {
         store.deleteCachedFeed { [weak self] error in
             guard let self = self else { return }
 
@@ -29,9 +31,9 @@ public class LocalFeedLoader {
         }
     }
     
-    func cache(_ items: [FeedItem], with completion: @escaping (Error?) -> Void) {
+    func cache(_ items: [FeedItem], with completion: @escaping (SaveResult) -> Void) {
         store.insert(items, timestamp: self.currentDate()) { [weak self] error in
-            guard let self = self else { return }
+            guard self != nil else { return }
             completion(error)
         }
     }
