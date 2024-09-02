fd66d799a644defcef297cb5c0dfdde0ee9d7506
ChenZhen <chenzhennba@gmail.com>
2024-09-02 20:38:55 +1000

    Create FeedStoreSpecs that must be implemented by any FeedStore test implementation to guarantee it meets spec

---
+++ EssentialFeed/EssentialFeedTests/Feed Cache/FeedStoreSpecs.swift
@@ -0,0 +1,49 @@
+//
+//  FeedStoreSpecs.swift
+//  EssentialFeedTests
+//
+//  Created by ChenZhen on 2/9/24.
+//
+
+import Foundation
+
+protocol FeedStoreSpecs {
+    func test_retrieve_deliverEmptyOnEmptyCache()
+
+    func test_retrieve_hasNoSideEffectsOnEmptyCache()
+
+    func test_retrieve_hasNoSideEffectsOnNonEmptyCache()
+
+    func test_retrieve_deliversFoundValuesOnNonEmptyCache()
+
+    func test_insert_deliverNoErrorOnEmptyCache()
+
+    func test_insert_deliversNoErrorOnNonEmptyCache()
+
+    func test_insert_overridesPreviouslyInsertedCacheValues()
+
+    func test_delete_deliversNoErrorOnEmptyCache()
+
+    func test_delete_emptiesPreviouslyInsertedCache()
+
+    func test_delete_deliversNoErrorOnNonEmptyCache()
+
+    func test_storeSideEffects_runSerially()
+}
+
+protocol FailableRetrieveFeedStoreSpecs: FeedStoreSpecs {
+    func test_retrieve_deliversFailureOnRetrievalError()
+    func test_retrieve_hasNoSideEffectsOnFailure()
+}
+
+protocol FailableInsertFeedStoreSpecs: FeedStoreSpecs {
+    func test_insert_deliversErrorOnInsertionError()
+    func test_insert_hasNoSideEffectsOnInsertionError()
+}
+
+protocol FailableDeleteFeedStoreSpecs: FeedStoreSpecs {
+    func test_delete_deliverErrorOnDeletionError()
+    func test_delete_hasNoSideEffectsOnDeletionError()
+}
+
+typealias FailableFeedStoreSpecs = FailableRetrieveFeedStoreSpecs & FailableInsertFeedStoreSpecs & FailableDeleteFeedStoreSpecs
