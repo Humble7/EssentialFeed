dcff091d16aac9d5b749a1e4b2b682b532b6e0b7
ChenZhen <chenzhennba@gmail.com>
2024-12-12 22:29:56 +1100

    Move FeedViewAdapter to separate file

--- EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -20,24 +20,6 @@ public final class FeedUIComposer {
             loadingView: WeakRefVirtualProxy(feedController))
         return feedController
     }
-    
-    private final class FeedViewAdapter: FeedView {
-        private weak var controller: FeedViewController?
-        private let imageLoader: FeedImageDataLoader
-
-        init(controller: FeedViewController, imageLoader: FeedImageDataLoader) {
-            self.controller = controller
-            self.imageLoader = imageLoader
-        }
-
-        func display(_ viewModel: FeedViewModel) {
-            controller?.tableModel = viewModel.feed.map { model in
-                FeedImageCellController(viewModel:
-                    FeedImageViewModel(model: model, imageLoader: imageLoader, imageTransformer: UIImage.init))
-            }
-        }
-    }
-
 }
 
 private extension FeedViewController {
---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedViewAdapter.swift
@@ -0,0 +1,26 @@
+//
+//  FeedViewAdapter.swift
+//  EssentialFeediOS
+//
+//  Created by ChenZhen on 12/12/24.
+//
+
+import UIKit
+import EssentialFeed
+
+final class FeedViewAdapter: FeedView {
+    private weak var controller: FeedViewController?
+    private let imageLoader: FeedImageDataLoader
+
+    init(controller: FeedViewController, imageLoader: FeedImageDataLoader) {
+        self.controller = controller
+        self.imageLoader = imageLoader
+    }
+
+    func display(_ viewModel: FeedViewModel) {
+        controller?.tableModel = viewModel.feed.map { model in
+            FeedImageCellController(viewModel:
+                FeedImageViewModel(model: model, imageLoader: imageLoader, imageTransformer: UIImage.init))
+        }
+    }
+}
