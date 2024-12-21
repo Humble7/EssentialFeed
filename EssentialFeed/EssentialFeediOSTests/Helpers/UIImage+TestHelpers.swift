ca37c96400989e00c19be1640d8a5c8df4adfdae
ChenZhen <chenzhennba@gmail.com>
2024-12-21 16:15:48 +1100

    Record empty feed snapshot

---
+++ EssentialFeed/EssentialFeediOSTests/Helpers/UIImage+TestHelpers.swift
@@ -0,0 +1,34 @@
+//
+//  UIImage+TestHelpers.swift
+//  EssentialFeediOSTests
+//
+//  Created by ChenZhen on 11/12/24.
+//
+
+import UIKit
+
+extension UIImage {
+    static func make(withColor color: UIColor) -> UIImage {
+        guard Thread.isMainThread else {
+            var res: UIImage?
+            DispatchQueue.main.sync {
+                res = self.make(withColor: color)
+            }
+            return res!
+        }
+        
+        let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
+        UIGraphicsBeginImageContext(rect.size)
+        let context = UIGraphicsGetCurrentContext()!
+        context.setFillColor(color.cgColor)
+        context.fill(rect)
+        let img = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        
+        let imv = UIImageView()
+        imv.image = UIImage(data: img!.pngData()!)
+        
+        return imv.image!
+    }
+}
