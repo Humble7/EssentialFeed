ee8cb51f14ed85a7db1b60f0fcc1c27fca0ae94c
ChenZhen <chenzhennba@gmail.com>
2024-12-25 16:20:38 +1100

    Create the FeedItem model

---
+++ practice3/EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by ChenZhen on 25/12/24.
+//
+
+import Foundation
+ 
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
