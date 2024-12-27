b7ef17a26a612b3aebc272938a0be27ddc235b99
ChenZhen <chenzhennba@gmail.com>
2024-12-27 22:49:06 +1100

    Move the URLSessionHTTPClient (the system under test, or "SUT") creation to a factory method to protect our tests from breaking changes.

--- practice3/EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ practice3/EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -41,7 +41,6 @@ final class URLSessionHTTPClientTests: XCTestCase {
     func test_getFromURL_performGetRequestWithURL() {
 
         let url = URL(string: "http://another-url.com")!
-        let sut = URLSessionHTTPClient()
         
         let exp = expectation(description: "Wait for request")
         URLProtocolStub.observeRequests { request in
@@ -50,7 +49,7 @@ final class URLSessionHTTPClientTests: XCTestCase {
             exp.fulfill()
         }
         
-        sut.get(from: url) { _ in }
+        makeSUT().get(from: url) { _ in }
                 
         wait(for: [exp], timeout: 1.0)
     }
@@ -61,10 +60,8 @@ final class URLSessionHTTPClientTests: XCTestCase {
         let error = NSError(domain: "test-error", code: -1)
         URLProtocolStub.stub(data: nil, response: nil, error: error)
 
-        let sut = URLSessionHTTPClient()
-
         let exp = expectation(description: "Wait for completion")
-        sut.get(from: url) { result in
+        makeSUT().get(from: url) { result in
             switch result {
             case let .failure(receivedError as NSError):
                 XCTAssertEqual(receivedError.domain, error.domain)
@@ -78,6 +75,12 @@ final class URLSessionHTTPClientTests: XCTestCase {
         
         wait(for: [exp], timeout: 1.0)
 
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT() -> URLSessionHTTPClient {
+        return URLSessionHTTPClient()
     }
     
     private class URLProtocolStub: URLProtocol {
