bd5a02ffbeae8cc606eb8045c6accbae35dbffae
ChenZhen <chenzhennba@gmail.com>
2025-01-18 23:26:10 +1100

    Move ViewModels to separate files

---
+++ practice3/EssentialFeed/EssentialFeediOS/Feed UI/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by ChenZhen on 18/1/25.
+//
+
+import Foundation
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ practice3/EssentialFeed/EssentialFeediOS/Feed UI/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by ChenZhen on 18/1/25.
+//
+
+import Foundation
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
