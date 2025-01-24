88c71c4b0a7f286f5c342aa3b0e8997749d37e26
ChenZhen <chenzhennba@gmail.com>
2025-01-25 10:40:40 +1100

    Move `FeedLoadingViewModel` to separate file

---
+++ practice3/EssentialFeed/EssentialFeed/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by ChenZhen on 25/1/25.
+//
+
+import Foundation
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
