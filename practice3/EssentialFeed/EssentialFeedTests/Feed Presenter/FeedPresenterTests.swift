cb8d4b398a211e6b580557971b946dda3fc872fe
ChenZhen <chenzhennba@gmail.com>
2025-01-25 08:57:53 +1100

    `FeedPresenter` displays loading on `didStartLoadingFeed`

--- practice3/EssentialFeed/EssentialFeedTests/Feed Presenter/FeedPresenterTests.swift
+++ practice3/EssentialFeed/EssentialFeedTests/Feed Presenter/FeedPresenterTests.swift
@@ -7,9 +7,23 @@
 
 import XCTest
 
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
+
+protocol FeedLoadingView {
+    func display(_ viewModel: FeedLoadingViewModel)
+}
+
 final class FeedPresenter {
-    init(view: Any) {
-        
+    private let feedLoadingView: FeedLoadingView
+
+    init(feedLoadingView: FeedLoadingView) {
+        self.feedLoadingView = feedLoadingView
+    }
+    
+    func didStartLoadingFeed() {
+        feedLoadingView.display(FeedLoadingViewModel(isLoading: true))
     }
 }
 
@@ -21,18 +35,34 @@ class FeedPresenterTests: XCTestCase {
         XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
     }
     
+    func test_start() {
+        let (sut, view) = makeSUT()
+        sut.didStartLoadingFeed()
+        
+        XCTAssertEqual(view.messages, [.dispaly(true)])
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: FeedPresenter, view: ViewSpy) {
         let view = ViewSpy()
-        let sut = FeedPresenter(view: view)
+        let sut = FeedPresenter(feedLoadingView: view)
         trackForMemoryLeaks(view)
         trackForMemoryLeaks(sut)
         
         return (sut, view)
     }
     
-    private class ViewSpy {
-        let messages = [Any]()
+    private class ViewSpy: FeedLoadingView {
+        func display(_ viewModel: FeedLoadingViewModel) {
+            messages.append(.dispaly(viewModel.isLoading))
+        }
+        
+        
+        enum Message: Equatable {
+            case dispaly(_ isLoading: Bool)
+        }
+        
+        var messages = [Message]()
     }
 }
