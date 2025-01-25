d89cf915c2ee002d98e8b905de3881f60fcab7ed
ChenZhen <chenzhennba@gmail.com>
2025-01-25 14:06:30 +1100

    Move FeedImageViewModel to separate file

---
+++ practice3/EssentialFeed/EssentialFeed/Feed Presentation/FeedImageViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  FeedImageViewModel.swift
+//  EssentialFeed
+//
+//  Created by ChenZhen on 25/1/25.
+//
+
+import Foundation
+
+public struct FeedImageViewModel<Image> {
+    public let description: String?
+    public let location: String?
+    public let image: Image?
+    public let isLoading: Bool
+    public let shouldRetry: Bool
+    
+    public var hasLocation: Bool {
+        return location != nil
+    }
+}
